ec354a78eba99d6297f14b371367cd82d2e363c4
Travis Wu <travis.wu@jh-tech.tw>
2020-09-21 14:56:06 +0800

    Add router and props data to page.

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,182 +1,68 @@
+import 'dart:html';
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: new HomePage2(),
+      initialRoute: "/",
+      routes: {
+        "/": (context) {
+          return MyApp();
+        },
+        "/page2": (context) {
+          return Page2();
+        },
+      },
+      // home: new MyApp(),
     ));
 
-class HomePage extends StatelessWidget {
-  int index = 0;
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    // debugPaintSizeEnabled = true;
-    return Scaffold(
-      appBar: new AppBar(title: Text('bottom navigation bar demo')),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 100,
-          childAspectRatio: 2 / 3,
-        ), // SliverGridDelegateWithFixedCrossAxisCount(
-        //   crossAxisCount: 2,
-        //   childAspectRatio: 2 / 3,
-        // ),
-        itemBuilder: (context, idx) {
-          return Card(
-              child: Container(
-            width: 500,
-            height: 500,
-            color: Colors.green,
-            child: Center(
-              child: Text(
-                'index:$idx',
-              ),
-            ),
-          ));
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: index,
-        onTap: (int index) {
-          index = index;
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.add,
-              color: Colors.black87,
-            ),
-            title: Text(
-              'Add',
-              style: TextStyle(
-                color: Colors.black87,
-              ),
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.account_circle,
-              color: Colors.black87,
-            ),
-            title: Text(
-              'Account',
-              style: TextStyle(
-                color: Colors.black87,
-              ),
-            ),
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.close,
-                color: Colors.black87,
-              ),
-              title: Text(
-                'Close',
-                style: TextStyle(
-                  color: Colors.black87,
-                ),
-              )),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.build,
-                color: Colors.black87,
-              ),
-              title: Text(
-                'Build',
-                style: TextStyle(
-                  color: Colors.black87,
-                ),
-              )),
-        ],
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('page1'),
+        ),
+        body: Container(
+          color: Colors.red,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Navigator.pushNamed(context, '/page2');
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              return Page2(textData: 'abcd');
+            }));
+          },
+        ),
       ),
     );
   }
 }
 
-class HomePage2 extends StatefulWidget {
-  HomePage2({Key key}) : super(key: key);
-
-  @override
-  _HomePage2State createState() => _HomePage2State();
-}
-
-class _HomePage2State extends State<HomePage2> {
-  int index = 0;
-  List<Widget> pages = [
-    Container(
-      color: Colors.red,
-    ),
-    Container(
-      color: Colors.green,
-    ),
-    Container(
-      color: Colors.amber,
-    ),
-    Container(
-      color: Colors.black87,
-    ),
-  ];
+class Page2 extends StatelessWidget {
+  final String textData;
+  Page2({Key key, @required this.textData}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(title: Text('bottom navigation bar demo')),
-      body: pages[index],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: index,
-        onTap: (int idx) {
-          setState(() {
-            index = idx;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.add,
-              color: Colors.black87,
-            ),
-            title: Text(
-              'Add',
-              style: TextStyle(
-                color: Colors.black87,
-              ),
-            ),
+    return Container(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text('page2'),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.account_circle,
-              color: Colors.black87,
+          body: SizedBox.expand(
+            child: Container(
+              color: Colors.green,
+              child: Text(textData),
             ),
-            title: Text(
-              'Account',
-              style: TextStyle(
-                color: Colors.black87,
-              ),
-            ),
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.close,
-                color: Colors.black87,
-              ),
-              title: Text(
-                'Close',
-                style: TextStyle(
-                  color: Colors.black87,
-                ),
-              )),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.build,
-                color: Colors.black87,
-              ),
-              title: Text(
-                'Build',
-                style: TextStyle(
-                  color: Colors.black87,
-                ),
-              )),
-        ],
-      ),
+          )),
     );
   }
 }
